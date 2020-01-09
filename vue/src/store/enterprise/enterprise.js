3f00f62c9b351c2e27eed930a5ce08f9151bb6d5
Kwon Hye-ryung <leja0418@gmail.com>
2020-01-09 11:22:11 +0900

    update Store
    update Store
    add store dir.

---
+++ vue/src/store/enterprise/enterprise.js
@@ -0,0 +1,12 @@
+const state = {}
+const getters = {}
+const actions = {}
+const mutations = {}
+export default {
+    name: 'enterprise',
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
