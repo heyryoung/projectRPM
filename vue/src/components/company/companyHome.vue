91c4992204a0fc283d56edd9fea756a0c8d7acd0
seongjo24 <53929443+seongjo24@users.noreply.github.com>
2020-01-09 13:07:41 +0900

    first
    first 첫추가

---
+++ vue/src/components/company/companyHome.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+    <h1>-----------------------------------------------------------------</h1>
+    <a href="/cmain">|| main  ||</a>
+    <a href="/customerList"> 손님 목록 ||</a>
+    <a href="/carList"> 보유차량  ||</a>
+    <a href="/chart"> 차트 예  ||</a>
+    <h1>-----------------------------------------------------------------</h1>
+
+<router-view name="company"></router-view>
+</div>
+</template>
---
+++ vue/src/components/recommend/recommendHome.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<template>
+<component :is="'Tab'"></component>
+</template>
+<template>
+
+<router-view name="content"/>
+
+</template>
+</div>
+</template>
+<script>
+
+import Tab from "@/components/recommend/recommendTab.vue"
+
+export default{
+	components:{Tab}
+}
+</script>
+<style scoped></style>
